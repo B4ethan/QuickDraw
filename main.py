ac4847d5e3ee92e186eaa7c67cbba419f85506a9
B4ethan <eitan.lyakas@gmail.com>
2024-01-04 11:12:48 +0200

    change something i will be back!

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ class DrawingBoardGraphics:
 
         self.buttons()
 
+        self.root.protocol("WM_DELETE_WINDOW", self.onClose)
+        self.root.attributes("-topmost", True)
+        self.root.mainloop()
+
     def buttons(self):
         btnFrame = tk.Frame(self.root)
         btnFrame.pack(fill = tk.X, side = tk.BOTTOM)
@@ -44,5 +48,55 @@ class DrawingBoardGraphics:
         btnFrame.columnconfigure(1, weight = 1)
         btnFrame.columnconfigure(2, weight = 1)
 
+        clearBtn = tk.Button(btnFrame, text = "Clear Drawing", command = self.clearDrawing)
+        clearBtn.grid(row = 0, column = 0, sticky = tk.W + tk.E)
+        clearBtn.config(font = ("Century Gothic", 12))
+
+        saveDrawingBtn = tk.Button(btnFrame, text = "Save Drawing", command = lambda: self.saveDrawing())
+        saveDrawingBtn.grid(row = 0, column = 1, sticky = tk.W + tk.E)
+        saveDrawingBtn.config(font = ("Century Gothic", 12))
+
+        trainModelBtn = tk.Button(btnFrame, text = "Train Model", command = self.trainModel)
+        trainModelBtn.grid(row = 0, column = 2, sticky = tk.W + tk.E)
+        trainModelBtn.config(font = ("Century Gothic", 12))
+
+        predictDrawing = tk.Button(btnFrame, text = "Predict Drawing", command = self.predictDrawing)
+        predictDrawing.grid(row = 1, column = 1, sticky = tk.W + tk.E)
+        predictDrawing.config(font = ("Century Gothic", 12))
+
+        self.statusLabel = tk.Label(btnFrame, text = "Haha I'm smart ROBOT")
+        self.statusLabel.config(font = ("Century Gothic", 10))
+        self.statusLabel.grid(row = 2, column = 1, sticky = tk.W + tk.E)
+
+
     def paint(self):
         pass
+
+    def clearDrawing(self):
+        pass
+    
+    def saveDrawing(self):
+        pass
+
+    def trainModel(self):
+        self
+    
+    def predictDrawing(self):
+        pass
+    
+    def onClose(self):
+        pass
+
+class DrawingClassifeirCnnModel:
+    def __init__(self):
+        model = keras.Sequential([
+            
+        ])
+
+
+
+def main():
+    DrawingBoardGraphics()
+
+if __name__ == "__main__":
+    main()
